7794ab4ed563ba823943f709ea5f4e8b0c399f03
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-11-06 12:40:34 +0800

    redux applied

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,28 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  Route, 
-  RouterProvider 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
 } from 'react-router-dom'
 import HomePages from './pages/HomePages.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import { Provider } from 'react-redux';
+import store from './store.js'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={ <App /> }>
-      <Route index={ true } path='/' element={ <HomePages /> } />
-      <Route path='/login' element={ <LoginPage /> } />
-      <Route path='/register' element={ <RegisterPage /> } />
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePages />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 )
